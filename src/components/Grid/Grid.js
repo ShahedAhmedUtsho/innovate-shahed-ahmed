273ab2ea2541dbae4d6b04eaf023178415dd55fe
Shahed Ahmed <shahedahmedutsho@gmail.com>
2024-09-24 23:48:14 +0600

    push grid

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -14,24 +14,24 @@ const Grid = () => {
 
 
                 <div className='    flex flex-col gap-5 '>
-                    <span className='   w-[430px] min-h-[420px] border-2 border-black rounded-[30px]  '>
+                    <span className='  min-w-[46vw] min-h-[46vw] lg:min-w-[26vw] lg:min-h-[26vw] border-2 border-black lg:rounded-[30px] md:rounded-3xl rounded-xl  '>
 
                     </span>
                     <div className=' flex gap-4 2 h-full mt-auto'>
                    
-<span className='border-2 border-black rounded-[30px] h-32 w-32'></span>
-<span className='border-2 border-black rounded-[30px] h-32 w-32'></span>
-<span className='border-2 border-black rounded-[30px] h-32 w-32'></span>
+<span className='border-2 border-black lg:rounded-[30px] md:rounded-3xl rounded-xl lg:h-[8vw] lg:w-[8vw] h-[14vw] w-[14vw] '></span>
+<span className='border-2 border-black lg:rounded-[30px] md:rounded-3xl rounded-xl lg:h-[8vw] lg:w-[8vw] h-[14vw] w-[14vw]'></span>
+<span className='border-2 border-black lg:rounded-[30px] md:rounded-3xl rounded-xl lg:h-[8vw] lg:w-[8vw] h-[14vw] w-[14vw]'></span>
                     </div>
                 </div>
 
 
 
                 <div className='  w-full flex flex-col gap-4'> 
-<span className='border-2 border-black rounded-[30px] h-32 w-32'></span>
-<span className='border-2 border-black rounded-[30px] h-32 w-32'></span>
-<span className='border-2 border-black rounded-[30px] h-32 w-32'></span>
-<span className='border-2 border-black rounded-[30px] h-32 w-32'></span>
+<span className='border-2 border-black lg:rounded-[30px] md:rounded-3xl rounded-xl lg:h-[8vw] lg:w-[8vw] h-[14vw] w-[14vw]'></span>
+<span className='border-2 border-black lg:rounded-[30px] md:rounded-3xl rounded-xl lg:h-[8vw] lg:w-[8vw] h-[14vw] w-[14vw] '></span>
+<span className='border-2 border-black lg:rounded-[30px] md:rounded-3xl rounded-xl lg:h-[8vw] lg:w-[8vw] h-[14vw] w-[14vw]'></span>
+<span className='border-2 border-black lg:rounded-[30px] md:rounded-3xl rounded-xl lg:h-[8vw] lg:w-[8vw] h-[14vw] w-[14vw]'></span>
 
                 </div>
 
@@ -41,11 +41,11 @@ const Grid = () => {
 
             <div className=' w-full flex  items-center'>
                 
-            <div className='max-w-[430px]'>
-                        <h3 className='text-[40px] font-bold mb-3 '>
+            <div className='max-w-[430px] lg:ml-0 ml-[5vw] '>
+                        <h3 className='lg:text-[40px] text-3xl  font-bold mb-3 '>
                         Collection Featured
                         </h3>
-                        <p className='text-2xl '>
+                        <p className='lg:text-2xl text-xl '>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                         </p>
                     </div>
